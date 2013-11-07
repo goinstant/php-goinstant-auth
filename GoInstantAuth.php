2a89c07941aa9c206fb01034dc209c02eb4d6f2d
Jeremy Stashewsky <jeremy@goinstant.com>
2013-11-06 17:45:32 -0800

    Signing functionally complete

--- GoInstantAuth.php
+++ GoInstantAuth.php
@@ -1,13 +1,164 @@
 <?php
 class GoInstantAuth {
 
-  private $_binaryKey = null;
+  /// Class Members ///
 
-  public function setKey($key) {
+  public static function base64url_encode($str) {
+    $str = base64_encode($str);
+    if ($str === false) {
+      throw new Exception('GoInstantAuth: base64url encoding failed');
+    }
+
+    $str = str_replace('=','',$str);
+    $str = str_replace('/','_',$str);
+    $str = str_replace('+','-',$str);
+    return $str;
   }
 
-  public function sign($claims) {
-    return "a.b.c";
+  public static function base64url_decode($str) {
+    $str = trim($str);
+    $str = str_replace('_','/',$str);
+    $str = str_replace('-','+',$str);
+    return base64_decode($str, true);
+  }
+
+  public static function compact_encode($o) {
+    if (defined('JSON_UNESCAPED_SLASHES')) {
+      $json = json_encode($o, JSON_UNESCAPED_SLASHES);
+    } else {
+      $json = json_encode($o);
+      if ($json !== false) {
+        $json = str_replace('\\/','/', $json);
+      }
+    }
+
+    if ($json === false) {
+      throw new Exception('JSON encoding failed: '.json_last_error_msg());
+    }
+
+    return GoInstantAuth::base64url_encode($json);
+  }
+
+  public static function compact_decode($str) {
+    $json = GoInstantAuth::base64url_decode($str);
+    if ($json === false) {
+      return false;
+    }
+    return json_decode($json, true);
+  }
+
+  static function mapRequiredClaims(&$claims, &$table, $msg='missing required key') {
+    foreach($table as $name => $jwtClaim) {
+      if (!isset($claims[$name])) {
+        throw new InvalidArgumentException($msg.': '.$name);
+      }
+      $tmp = $claims[$name];
+      unset($claims[$name]);
+      $claims[$jwtClaim] = $tmp;
+    }
+  }
+
+  static function mapOptionalClaims(&$claims, &$table) {
+    foreach($table as $name => $jwtClaim) {
+      if (!isset($claims[$name])) {
+        continue;
+      }
+      $tmp = $claims[$name];
+      unset($claims[$name]);
+      $claims[$jwtClaim] = $tmp;
+    }
+  }
+
+  /// Instance Members ///
+
+  private $_binaryKey = null;
+
+  public function __construct($secretKey) {
+    if (is_null($secretKey) || !is_string($secretKey)) {
+      throw new InvalidArgumentException('Secret Key must be a string');
+    }
+
+    $this->_binaryKey = GoInstantAuth::base64url_decode($secretKey);
+    $secretKey = trim($secretKey);
+
+    // convert from base64url
+    $secretKey = str_replace('_','/',$secretKey);
+    $secretKey = str_replace('-','+',$secretKey);
+
+    $this->_binaryKey = base64_decode($secretKey, true);
+    if ($this->_binaryKey === false) {
+      throw new InvalidArgumentException('Secret Key must be base64 or base64url');
+    }
+  }
+
+  function userDataToClaims($userData) {
+    if (is_null($userData) || !is_array($userData)) {
+      throw new InvalidArgumentException('userData must be an array');
+    }
+
+    $claims = $userData; // copy
+
+    // required claims
+    $REQUIRED = array(
+      'id' => 'sub',
+      'domain' => 'iss',
+      'displayName' => 'dn'
+    );
+    $OPTIONAL = array(
+      'groups' => 'g'
+    );
+
+    $GROUP_REQUIRED = array(
+      'id' => 'id',
+      'displayName' => 'dn'
+    );
+
+    GoInstantAuth::mapRequiredClaims($claims, $REQUIRED);
+    GoInstantAuth::mapOptionalClaims($claims, $OPTIONAL);
+
+    // clean up groups sub-structure
+
+    if (isset($claims['g'])) {
+      if (!is_array($claims['g'])) {
+        throw new InvalidArgumentException(
+          'optional "groups" key must be an array'
+        );
+      }
+
+      foreach ($claims['g'] as $i => $group) {
+        $group = $group; // clone
+        GoInstantAuth::mapRequiredClaims(
+          $group,
+          $GROUP_REQUIRED,
+          "group $i missing required key"
+        );
+        $claims['g'][$i] = $group;
+      }
+    }
+
+    return $claims;
+  }
+
+  public function sign($userData, $extraHeader=array()) {
+    $claims = $this->userDataToClaims($userData);
+
+    $header = $extraHeader; // copy
+    // overwrite:
+    $header['typ'] = 'JWT';
+    $header['alg'] = 'HS256';
+
+    $jwsProtectedHeader = GoInstantAuth::compact_encode($header);
+    $jwsPayload = GoInstantAuth::compact_encode($claims);
+    $jwsSigningInput = $jwsProtectedHeader . '.' . $jwsPayload;
+
+    $sig = GoInstantAuth::base64url_encode(hash_hmac(
+      'sha256',
+      $jwsSigningInput,
+      $this->_binaryKey,
+      true
+    ));
+
+    return $jwsSigningInput . '.' . $sig;
   }
 }
 ?>
---
+++ Tests/GoInstantAuthTest.php
@@ -0,0 +1,181 @@
+<?php
+
+require_once 'GoInstantAuth.php';
+
+/**
+ * Test constructor (which checks JWS parameters like keys).
+ */
+class ConstructorTest extends PHPUnit_Framework_TestCase {
+
+  public function testNoKeySet() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'Secret Key must be a string'
+    );
+    new GoInstantAuth(NULL);
+  }
+
+  public function testNotBase64() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'Secret Key must be base64 or base64url'
+    );
+    new GoInstantAuth('$%^');
+  }
+
+  public function testNoBase64Padding() {
+    // "GoInsta" -> "R29JbnN0YW=="
+    $auth = new GoInstantAuth('R29JbnN0YW');
+    $this->assertInstanceOf('GoInstantAuth', $auth);
+  }
+}
+
+/**
+ * Test token signature/creation.
+ */
+class SignTest extends PHPUnit_Framework_TestCase {
+
+  private function validateJwt($jwt, $expectClaims=array()) {
+    $parts = explode('.', $jwt);
+    $this->assertCount(3, $parts);
+    $this->assertRegExp('/^[a-z0-9_\-]+$/i', $parts[0]);
+    $this->assertRegExp('/^[a-z0-9_\-]+$/i', $parts[1]);
+    // since we currently only support HS256, the signature part is fixed-size:
+    $this->assertRegExp('/^[a-z0-9_\-]{43}$/i', $parts[2]);
+
+    $header = GoInstantAuth::compact_decode($parts[0]);
+    $this->assertEquals(2, count($header));
+    $this->assertEquals('JWT', $header['typ']);
+    $this->assertEquals('HS256', $header['alg']);
+
+    $claims = GoInstantAuth::compact_decode($parts[1]);
+    $this->assertEquals($expectClaims, $claims);
+  }
+
+  protected function setUp() {
+    $this->auth = new GoInstantAuth('HKYdFdnezle2yrI2_Ph3cHz144bISk-cvuAbeBNB030');
+  }
+
+  public function testNoClaims() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'userData must be an array'
+    );
+
+    $this->auth->sign(null);
+  }
+
+  public function testIdMissing() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'missing required key: id'
+    );
+
+    $this->auth->sign(array(
+      'domain' => 'example.com',
+      'displayName' => 'bob'
+    ));
+  }
+
+  public function testDomainMissing() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'missing required key: domain'
+    );
+
+    $this->auth->sign(array(
+      'id' => 'bar',
+      'displayName' => 'bob'
+    ));
+  }
+
+  public function testDisplayNameMissing() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'missing required key: displayName'
+    );
+
+    $this->auth->sign(array(
+      'id' => 'bar',
+      'domain' => 'example.com'
+    ));
+  }
+
+  public function testGroupsNotArray() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'optional "groups" key must be an array'
+    );
+
+    $this->auth->sign(array(
+      'id' => 'bar',
+      'domain' => 'example.com',
+      'displayName' => 'bob',
+      'groups' => 'none'
+    ));
+  }
+
+  public function testHappyNoGroups() {
+    $jwt = $this->auth->sign(array(
+      'id' => 'bar',
+      'domain' => 'example.com',
+      'displayName' => 'Bob',
+      'groups' => array()
+    ));
+
+    $this->validateJwt($jwt, array(
+      'sub' => 'bar',
+      'iss' => 'example.com',
+      'dn' => 'Bob',
+      'g' => array()
+    ));
+  }
+
+  public function testGroupMissingId() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'group 0 missing required key: id'
+    );
+
+    $this->auth->sign(array(
+      'id' => 'bar',
+      'domain' => 'example.com',
+      'displayName' => 'Bob',
+      'groups' => array(
+        array('displayName' => 'MyGroup')
+      )
+    ));
+  }
+
+  public function testGroupMissingDisplayName() {
+    $this->setExpectedException(
+      'InvalidArgumentException', 'group 1 missing required key: displayName'
+    );
+
+    $this->auth->sign(array(
+      'id' => 'bar',
+      'domain' => 'example.com',
+      'displayName' => 'Bob',
+      'groups' => array(
+        array('id' => 99, 'displayName' => 'Gretzky Lovers'),
+        array('id' => 1234)
+      )
+    ));
+  }
+
+  public function testHappyGroups() {
+    $jwt = $this->auth->sign(array(
+      'id' => 'bar',
+      'domain' => 'example.com',
+      'displayName' => 'Bob',
+      'groups' => array(
+        array('id' => 1234, 'displayName' => 'Group 1234'),
+        array('id' => 42, 'displayName' => 'Meaning Group')
+      )
+    ));
+
+    $this->validateJwt($jwt, array(
+      'sub' => 'bar',
+      'iss' => 'example.com',
+      'dn' => 'Bob',
+      'g' => array(
+        array('id' => 1234, 'dn' => 'Group 1234'),
+        array('id' => 42, 'dn' => 'Meaning Group')
+      )
+    ));
+  }
+}
+
+?>
