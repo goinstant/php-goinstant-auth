e276234d3004b87b9c13937a08e2e3dd4306a6a4
Jeremy Stashewsky <jeremy@goinstant.com>
2013-11-06 18:24:28 -0800

    Move class members down to the bottom

--- GoInstantAuth.php
+++ GoInstantAuth.php
@@ -1,74 +1,6 @@
 <?php
 class GoInstantAuth {
 
-  /// Class Members ///
-
-  public static function base64url_encode($str) {
-    $str = base64_encode($str);
-    if ($str === false) {
-      throw new Exception('GoInstantAuth: base64url encoding failed');
-    }
-
-    $str = str_replace('=','',$str);
-    $str = str_replace('/','_',$str);
-    $str = str_replace('+','-',$str);
-    return $str;
-  }
-
-  public static function base64url_decode($str) {
-    $str = trim($str);
-    $str = str_replace('_','/',$str);
-    $str = str_replace('-','+',$str);
-    return base64_decode($str, true);
-  }
-
-  public static function compact_encode($o) {
-    if (defined('JSON_UNESCAPED_SLASHES')) {
-      $json = json_encode($o, JSON_UNESCAPED_SLASHES);
-    } else {
-      $json = json_encode($o);
-      if ($json !== false) {
-        $json = str_replace('\\/','/', $json);
-      }
-    }
-
-    if ($json === false) {
-      throw new Exception('JSON encoding failed: '.json_last_error_msg());
-    }
-
-    return GoInstantAuth::base64url_encode($json);
-  }
-
-  public static function compact_decode($str) {
-    $json = GoInstantAuth::base64url_decode($str);
-    if ($json === false) {
-      return false;
-    }
-    return json_decode($json, true);
-  }
-
-  static function mapRequiredClaims(&$claims, &$table, $msg='missing required key') {
-    foreach($table as $name => $jwtClaim) {
-      if (!isset($claims[$name])) {
-        throw new InvalidArgumentException($msg.': '.$name);
-      }
-      $tmp = $claims[$name];
-      unset($claims[$name]);
-      $claims[$jwtClaim] = $tmp;
-    }
-  }
-
-  static function mapOptionalClaims(&$claims, &$table) {
-    foreach($table as $name => $jwtClaim) {
-      if (!isset($claims[$name])) {
-        continue;
-      }
-      $tmp = $claims[$name];
-      unset($claims[$name]);
-      $claims[$jwtClaim] = $tmp;
-    }
-  }
-
   /// Instance Members ///
 
   private $_binaryKey = null;
@@ -159,6 +91,74 @@ class GoInstantAuth {
     ));
 
     return $jwsSigningInput . '.' . $sig;
+  }
+
+  /// Class Members ///
+
+  public static function base64url_encode($str) {
+    $str = base64_encode($str);
+    if ($str === false) {
+      throw new Exception('GoInstantAuth: base64url encoding failed');
+    }
+
+    $str = str_replace('=','',$str);
+    $str = str_replace('/','_',$str);
+    $str = str_replace('+','-',$str);
+    return $str;
+  }
+
+  public static function base64url_decode($str) {
+    $str = trim($str);
+    $str = str_replace('_','/',$str);
+    $str = str_replace('-','+',$str);
+    return base64_decode($str, true);
+  }
+
+  public static function compact_encode($o) {
+    if (defined('JSON_UNESCAPED_SLASHES')) {
+      $json = json_encode($o, JSON_UNESCAPED_SLASHES);
+    } else {
+      $json = json_encode($o);
+      if ($json !== false) {
+        $json = str_replace('\\/','/', $json);
+      }
+    }
+
+    if ($json === false) {
+      throw new Exception('JSON encoding failed: '.json_last_error_msg());
+    }
+
+    return GoInstantAuth::base64url_encode($json);
+  }
+
+  public static function compact_decode($str) {
+    $json = GoInstantAuth::base64url_decode($str);
+    if ($json === false) {
+      return false;
+    }
+    return json_decode($json, true);
+  }
+
+  static function mapRequiredClaims(&$claims, &$table, $msg='missing required key') {
+    foreach($table as $name => $jwtClaim) {
+      if (!isset($claims[$name])) {
+        throw new InvalidArgumentException($msg.': '.$name);
+      }
+      $tmp = $claims[$name];
+      unset($claims[$name]);
+      $claims[$jwtClaim] = $tmp;
+    }
+  }
+
+  static function mapOptionalClaims(&$claims, &$table) {
+    foreach($table as $name => $jwtClaim) {
+      if (!isset($claims[$name])) {
+        continue;
+      }
+      $tmp = $claims[$name];
+      unset($claims[$name]);
+      $claims[$jwtClaim] = $tmp;
+    }
   }
 }
 ?>
